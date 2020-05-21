7f7926f82895f24c6376cb594de985ba298909b8
P-Jag <pavel.jagla@gmail.com>
2020-05-21 12:26:09 +0200

    OOP scroller

--- Projects/ProScroll/main.js
+++ Projects/ProScroll/main.js
@@ -1,34 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("Whats up man");
+  const scroller = new Scroller("#root");
 
-  const rootElement = document.querySelector("#root");
-  const sections = document.querySelectorAll("section");
-  let currentSectionIndex = 0;
-  let isThrottled = false;
-
-  document.addEventListener("mousewheel", function (event) {
-    if (isThrottled) return;
-    isThrottled = true;
-
-    setTimeout(function () {
-      isThrottled = false;
-    }, 1000);
-
-    const direction = event.wheelDelta < 0 ? 1 : -1;
-
-    if (direction === 1) {
-      const isLastSection = currentSectionIndex === sections.length - 1;
-      if (isLastSection) return;
-    } else if (direction === -1) {
-      const firstSection = currentSectionIndex === 0;
-      if (firstSection) return;
-    }
-
-    currentSectionIndex += direction;
-
-    sections[currentSectionIndex].scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  });
+  document.addEventListener("mousewheel", scroller.listenScroll);
 });
